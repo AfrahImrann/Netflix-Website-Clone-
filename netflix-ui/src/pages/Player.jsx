4c925024b333f0c50388d6f421bd0c854bdedd06
AfrahImrann <149326565+AfrahImrann@users.noreply.github.com>
2024-06-20 01:14:20 +0500

    Update Player.jsx

--- netflix-ui/src/pages/Player.jsx
+++ netflix-ui/src/pages/Player.jsx
@@ -1,39 +1,35 @@
-import React from "react";
-import styled from "styled-components";
-import { BsArrowLeft } from "react-icons/bs";
-import { useNavigate } from "react-router-dom";
-import video2 from "../assets/video2.mp4";
+import React, { useEffect, useContext } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useState } from 'react'
 
-export default function Player() {
-    const navigate = useNavigate();
-  return (
-    <Container>
-        <div className="player">
-            <div className="back">
-                <BsArrowLeft onClick={() => navigate(-1)}/>
-            </div>
-            <video src={video2} autoPlay loop controls></video>
-        </div>
-    </Container>
-  )
+const Player = () => {
+
+    const [moviedet, setMoviedet] = useState([]);
+    const { id } = useParams()
+
+    const APIKEY = ""
+    const fetchMovie = async () => {
+        const data = await fetch(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=${APIKEY}&language=en-US`
+        );
+        const moviedetail = await data.json();
+        setMoviedet(moviedetail);
+    };
+
+      useEffect(() => {
+        fetchMovie()
+ 
+      }, []);
+      
+    
+    document.title = `BlueBird Movies | ${moviedet.title}`
+
+    return (
+      <>
+      
+        <iframe allowFullScreen style={{ display: 'flex', alignItems:"center", justifyContent:"center", width:"100%", height:"100vh"}} src={`https://embed.smashystream.com/playere.php?tmdb=${id}`}></iframe>
+      </>
+    )
 }
 
-const Container = styled.div`
- .player {
-    height: 100vh;
-    width:100vw;
-    .back {
-        position: absolute;
-        padding: 2rem;
-        z-index: 1;
-    }
-    svg {
-        font-size: 3rem;
-        cursor: pointer;
-    }
-    video {
-        height: 100%;
-        width: 100%;
-        object-fit: center;
-    }
-}`;
+export default Player
